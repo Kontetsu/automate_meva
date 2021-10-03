b558bcbce006291b6f173302a3b1d7f59f125902
Kontetsu <57173347+Kontetsu@users.noreply.github.com>
2021-10-03 13:48:40 +0200

    For Testing

--- read_write_xml.ps1
+++ read_write_xml.ps1
@@ -1,31 +1,7 @@
-$FilePath = "C:\Users\Ergi\Documents\automate_meva\test.xml"
+$FilePath = "C:\Users\erget\OneDrive\Documents\automate_meva\test.xml"
 
-#Select-Xml -Path $Path -XPath "/catalog/book/author" | ForEach-Object { $_.Node.InnerXML }
+$XmlDocument = new-object System.Xml.XmlDocument
+[xml]$XmlDocument = Get-Content -Path $FilePath
 
+$XmlDocument.catalog.book.genre[11]
 
-
-#Select-Xml -Path $Path -XPath "/catalog/book/author" | Where-Object -Like {"price".$_Node.InnerXML}
-
-
-#[xml]$XmlDocument = Get-Content -Path C:\Users\Ergi\Documents\automate_meva\test.xml
-
-#$XmlDocument.catalog.book.price[0]
-
-$Selector = "//book[@id='bk101']"
-
-$NewTutorialTopic = "10"
-
-function write_to_xml()
-   {
-       
-      [xml]$XmlDocument = Get-Content -Path $FilePath
-      $XmlDocument.catalog.book[5].author = 'audubon, j.'
-      $XmlDocument.catalog.book[5].title = 'The Birds of America'
-      $XmlDocument.catalog.book[5].price = '50$'
-      $XmlDocument.Save("$FilePath") 
-
-      Write-Host "Saved into " $FilePath -ForegroundColor Green        
-
-   }
-
-write_to_xml
